7b800137780ad7dbea9eaf61c70834f907b7032b
makku26 <202311613@fit.edu.ph>
2024-07-10 00:17:43 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+//Loader
+window.addEventListener("load", () => {
+  const loader = document.querySelector(".loader");
+
+  // Simulate a delay of 3 seconds before hiding the loader
+  setTimeout(() => {
+    loader.classList.add("loader--hidden");
+  }, 800); // Adjust the time in milliseconds (3000 ms = 3 seconds)
+
+  loader.addEventListener("transitionend", () => {
+    document.body.removeChild(loader);
+  });
+});
+
+
 //For the LOGIN Page
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
